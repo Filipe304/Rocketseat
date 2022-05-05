212bd650a2a775738285fa9328bb4799a0419e74
Filipe304 <filipe.diablovt@hotmail.com>
2022-05-05 18:10:51 -0300

    Continuação do Projeto

--- Pratica 01/script.js
+++ Pratica 01/script.js
@@ -13,32 +13,31 @@ const Modal = {
     }
 }
 
-const transactions = [
-    {
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    }, 
-    {
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    },
-    {
-        description: 'App',
-        amount: 200000,
-        date: '23/01/2021'
-    },
-]
-
 const Transactions = {
-    all: transactions,
-    add(transactions) {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021'
+        }, 
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'App',
+            amount: 200000,
+            date: '23/01/2021'
+        },
+    ],
+    add(transactions) 
+    {
         Transactions.all.push(transactions)
 
         App.reload()
@@ -129,6 +128,17 @@ const DOM = {
 }
 
 const Utils = {
+    formatAmount(){
+        value = Number(value) * 100
+        
+        return value
+    },
+
+    formatDate(date){
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
 
@@ -143,6 +153,74 @@ const Utils = {
 
     return signal + value
     
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return{
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+        
+        if( description.trim() === "" ||
+            amount.trim() === "" ||
+            date.trim() === "") {
+                throw new error("Por favor, preencha todos os campos")
+            }
+
+},
+
+    formatValues() {
+        let { description, amount, date} = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    saveTransaction() {
+        Transactions.add(transaction)
+    },
+
+    clearFields(){
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+    
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Form.validateField()
+            // formatar os dados para salvar
+            const transaction = Form.formatData()
+            // salvar 
+            Form.saveTransaction()
+            // apagar os dados do formulario
+            Form.clearFields()
+            // modal feche
+            // atualizar a aplicação
+
+        } catch (error) {
+            alert(error.message)
+        }
     }
 }
 
